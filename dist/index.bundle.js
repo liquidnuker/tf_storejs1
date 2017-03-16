f4d2c3afd3c6fb3b14d079bb69e051a7908770d0
administrator <liquidnuker@users.noreply.github.com>
2017-03-16 14:12:49 -0800

    all methods ok. converting to module

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -76,11 +76,29 @@ var store = __webpack_require__(1);
 store.set('user1', { name: 'Marcus' });
 
 // get
-console.log(store.get('user1'));
+// console.log(store.get('user1'));
 
 // remove 
-store.remove("user1");
-console.log(store.get("user1"));
+// store.remove("user1");
+// console.log(store.get("user1"));
+
+store.set('user2', { name: 'Marcus2' });
+// console.log(store.get('user1'));
+
+// store.each(function(value, key) {
+//     console.log(key, '==', value)
+// });
+
+// Clear all keys
+// store.clearAll();
+
+var x = store.get("user2");
+console.log(x);
+
+var y = JSON.stringify(x);
+console.log(y);
+
+document.body.innerHTML = y;
 
 /***/ }),
 /* 1 */
--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ const store = require("./js/vendor/store.everything.min.js");
 store.set('user1', { name:'Marcus' });
 
 // get
-console.log(store.get('user1'));
+// console.log(store.get('user1'));
 
 // remove 
-store.remove("user1");
-console.log(store.get("user1"));
+// store.remove("user1");
+// console.log(store.get("user1"));
 
+store.set('user2', { name:'Marcus2' });
+// console.log(store.get('user1'));
 
+// store.each(function(value, key) {
+//     console.log(key, '==', value)
+// });
 
+// Clear all keys
+// store.clearAll();
+
+var x = store.get("user2");
+console.log(x);
+
+var y = JSON.stringify(x);
+console.log(y);
+
+document.body.innerHTML = y;
