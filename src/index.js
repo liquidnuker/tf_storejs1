ab19b38ea7226519533fb7bfbda7aebdc37858c2
administrator <liquidnuker@users.noreply.github.com>
2017-03-16 13:53:48 -0800

    getset ok

--- src/index.js
+++ src/index.js
@@ -1,41 +1,7 @@
-import {cookieToggle} from "./js/cookieToggle.js"
+const store = require("./js/vendor/store.everything.min.js");
 
+// set
+store.set('user1', { name:'Marcus' });
 
-// cookieToggle.set('name1', 'value1', { expires: 7 });
-// Cookies.set('name', 'value', { expires: 7, path: '' });
-// cookieToggle.remove("name1", "value1");
-
-// console.log(cookieToggle.get());
-// console.log(cookieToggle.get("name1"));
-
-// cookieToggle.set('name', 'value', { path: '' });
-// cookieToggle.remove('name'); // undef
-// cookieToggle.remove('name', { path: '' }); // removed!
-
-// console.log(cookieToggle.getJSON()); // => { name: { foo: 'bar' } }
-// console.log(cookieToggle.getJSON("name1")); // => { foo: 'bar' }
-
-let lastViewedItems = [];
-
-cookieToggle.set("name1", "value1");
-lastViewedItems.push(cookieToggle.getJSON("name1"));
-console.log(lastViewedItems);
-
-cookieToggle.set("name2", "value2");
-lastViewedItems.push(cookieToggle.getJSON("name2"));
-console.log(lastViewedItems);
-
-document.body.innerHTML = lastViewedItems;
-// document.body.innerHTML = lastViewedItems.toString().toUpperCase();
-
-// cookieToggle.set(lastViewedItems.toString());
-console.log(cookieToggle.getJSON());
-// document.body.innerHTML = lastViewedItems[1];
-// document.body.innerHTML = lastViewedItems.length;
-
-// document.body.innerHTML = x.name1; // value1
-// document.body.innerHTML = x.value1; // undef
-
-
-
-
+// get
+console.log(store.get('user1'));
